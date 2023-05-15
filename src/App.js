89e117b4620d3092b5c8eb6ddb811de8ef36eb31
Gustavo <gustavomeinert@gmail.com>
2023-05-15 09:49:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+
+  const [endereco, setEndereco] = useState({})
+
+  function manipularEndereco(evento) {
+
+    const cep = evento.target.value
+
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8) {
+      //obter o cep
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco(enderecoAntigo => ({
+            ...enderecoAntigo,
+            logradouro: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          }))
+        })
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder='Digite o cep' onChange={manipularEndereco}></input>
+        <ul>
+          <li>CEP: {endereco.cep}</li>
+          <li>Logradouro: {endereco.logradouro}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
